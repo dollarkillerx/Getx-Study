f8bddc71b7d49604ad0f39287861d0e1db4dbd57
dollarkillerx <adapawang@gmail.com>
2022-07-25 12:31:48 +0800

    init project

--- lib/pages/login/index.dart
+++ lib/pages/login/index.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-import '../../common/routes/app_routes.dart';
+class LoginView extends StatefulWidget {
+  @override
+  _LoginViewState createState() => _LoginViewState();
+}
 
-class LoginView extends StatelessWidget {
-  const LoginView({Key? key}) : super(key: key);
+class _LoginViewState extends State<LoginView> {
+  var userName = new TextEditingController();
+  var password = new TextEditingController();
+  var jc = new TextEditingController();
+
+  /// 设置Sting的值
+  static setString(String key, String value) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(key, value);
+  }
+
+  /// 获取返回为String的内容
+  getString(String key) async {
+    final prefs = await SharedPreferences.getInstance();
+    String? value = prefs.getString(key);
+    return value;
+  }
+
+  _load() async {
+    var jcv = await getString("jc");
+    if (jcv != null) {
+      setState(() {
+        jc.text = jcv!;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+      super.initState();
+      Future.delayed(Duration.zero, () => setState(() {
+        _load();
+      }));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +47,36 @@ class LoginView extends StatelessWidget {
       appBar: AppBar(
         title: Text("登录"),
       ),
-      body: ListTile(
-        title: Text("返回首页"),
-        subtitle: Text('Get.offAllNamed(AppRoutes.Home)'),
-        onTap: () => Get.offAllNamed(AppRoutes.Home),
+      body: ListView(
+        children: <Widget>[
+          TextField(
+            controller: userName,
+            decoration: InputDecoration(
+              labelText: "UserName",
+            ),
+          ),
+          Divider(),
+          TextField(
+            controller: password,
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: "Password",
+            ),
+          ),
+          Divider(),
+          TextField(
+            controller: jc,
+            decoration: InputDecoration(
+              labelText: "JC",
+            ),
+          ),
+          ElevatedButton(onPressed: () {
+            setState(() {
+              jc.text = userName.text + password.text;
+              setString("jc", jc.text);
+            });
+          }, child: Text("Login")),
+        ],
       ),
     );
   }
